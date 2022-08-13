949c28a2f12473cb5f6398eb8f929b830d6265b3
Kwonkunkun <kunwoo5012@naver.com>
2022-08-13 19:11:27 +0900

    서비스 워커 old cache 삭제 기능 추가

---
+++ sw_cached__pages.js
@@ -0,0 +1,38 @@
+const cacheName = "v1";
+
+const cacheAssets = ["index.html", "about.html", "main.js"];
+
+// install event
+self.addEventListener("install", (e) => {
+  console.log("Service worker: installed");
+
+  e.waitUntil(
+    caches
+      .open(cacheName)
+      .then((cache) => {
+        console.log("Service worker: Caching files");
+        cache.addAll(cacheAssets);
+      })
+      .then(() => {
+        self.skipWaiting();
+      })
+  );
+});
+
+// activate event
+self.addEventListener("activate", (e) => {
+  console.log("Service worker: activated");
+  //remove unwanted caches
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== cacheName) {
+            console.log("service worker: clearing old cache");
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
