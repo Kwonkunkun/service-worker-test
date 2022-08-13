3d2ccfab2bf730ad1e4ed548b04c2798956d403b
Kwonkunkun <kunwoo5012@naver.com>
2022-08-13 18:35:03 +0900

    서비스 워커 script browser에 등록

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-// make sure sw are supported
+// 서비스 워커가 이 브라우저에서 지원되면
 if ("serviceWorker" in navigator) {
-  console.log("Service worker supported");
+  // 서비스 워커 script 등록하기
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("./sw_cached__pages.js")
+      .then((reg) => {
+        console.log("service worker: registered");
+      })
+      .catch((err) => console.log(`service worker: error: ${err}`));
+  });
 }
